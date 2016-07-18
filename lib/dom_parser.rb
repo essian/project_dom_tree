fc949624403499674f65554baebf2f30dadc6fbc
essian <jessicampdennis@hotmail.com>
2016-07-18 23:12:16 +0100

    adds some code

--- lib/dom_parser.rb
+++ lib/dom_parser.rb
@@ -1,19 +1,27 @@
 Node = Struct.new(:data, :children, :parent)
 
 class DomParser
+  attr_accessor :root
   def initialize
     #@html = html
+    @root = nil
   end
 
   def parser_script(string)
-    @elements = string.scan(/((?<=>).*?(?=<)|<.*?>)/)
+    create_tree(string)
+    retrieve
+  end
+
+  def html_to_array(string)
+    # string.scan(/((?<=>).+?(?=<)|<.*?>)/)
+    string.scan(/(<.*?>|(?<=>).+?(?=<))/)
   end
 
   def parse_tag(string)
     tag = {}
 
     # save tag after < character
-    tag_type = string.match(/<(\w+)(?:>| )/)
+    tag_type = string.match(/<\/?(\w+)(?:>| )/)
 
     # saves either side of an equal sign
     attributes = string.scan(/(\w+)\s*=\s*['"](.*?)['"]/)
@@ -29,50 +37,69 @@ class DomParser
     tag
   end
 
-  def create_tree
-    parser_script(@html)
-    @root = create_node(@elements[0])
-    # current_node = @root
-    make_children(@root, 1)
+  def create_tree(string)
+    tags = html_to_array(string)
+    @root = create_node(tags[0])
+    current_node = @root
+    tags[1..-1].each do |tag|
+      node = create_node(tag)
+      if closing?(node)
+        break unless current_node.parent
+        current_node = current_node.parent
+
+      elsif tag?(node)
+        current_node.children << node
+        node.parent = current_node
+        current_node = node
+
+      elsif text?(node)
+        current_node.children << node
+        node.parent = current_node
+
+      end
+    end
+  end
+
+  def retrieve
+    stack = []
+    stack << @root
+    until stack.empty?
+      current_node = stack.pop
+      puts current_node.data
+      unless current_node.children.empty?
+        current_node.children.each { |child| stack.push(child)} unless current_node.children.empty?
+      end
+    end
   end
 
   def create_node(tag, parent = nil)
-    Node.new(tag, nil, parent)
+    Node.new(tag[0], [], parent)
+  end
+
+  def tag?(node)
+    node.data[0] == '<'
+  end
+
+  def text?(node)
+    !node.data.include?('<')
+  end
+
+  def closing?(node)
+    node.data[0..1] == "</"
   end
 
 
-  def make_children(parent, tag_index)
-
-    current_node = parent
 
 
+  # def opening_tag?(tag)
+  #   !closing_tag?(tag) && tag[0]=="<"
+  # end
 
-    # return if the next one is a closing tag?
-    loop do 
-      return if closing_tag_for?(@elements[tag_index, parent])
-      # if next one is a tag
-      if opening_tag?(@elements[tag_index])
-        create_node(parse_tag(@elements[tag_index]), parent)
-      end
-    end
-    
-    # create a node
-    # make_children(node)
-    # else
-    # create a node
-    # loops until we hit closing tag
-  end
-
-  def closing_tag_for?(current_tag, parent_node)
-    #check if current tag is closing_tag && current_tag.type = parent_node.type.
-
-  end
-
-  def opening_tag?(tag)
-    !closing_tag?(tag) && tag[0]=="<"
-  end
-
-  def closing_tag?(element)
-    element[1] == "/" 
-  end
+  # def closing_tag?(element)
+  #   element[1] == "/"
+  # end
 end
+
+d = DomParser.new
+html_string = "<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
+d.parser_script(html_string)
--- spec/dom_parser_spec.rb
+++ spec/dom_parser_spec.rb
@@ -1,27 +1,41 @@
 require 'dom_parser'
 
 describe DomParser do
+  let(:opening) {Node.new("<p>", nil, nil)}
+  let(:opening_dup) {Node.new("<p>", nil, nil)}
+  let(:closing) {Node.new("</p>", nil, nil)}
+  let(:text) {Node.new(" hello world ", nil, nil)}
 
-  describe "#parser_script" do
+  describe "#html_to_array" do
     it "converts a string to array of tags" do
       string = "<div>  div text before  <p>    p text  </p>  <div>    more div text  </div>  div text after</div>"
-      expect(subject.parser_script(string).length).to eq(11)
+      expect(subject.html_to_array(string).length).to eq(11)
     end
 
     it "parses a tag correctly" do
       string ="<div class='foo bar'>"
-      expect(subject.parser_script(string).first).to eq(["<div class='foo bar'>"])
+      expect(subject.html_to_array(string).first).to eq(["<div class='foo bar'>"])
     end
 
-    it "parses a text element" do 
+    it "parses a text element" do
       string ="<div class='foo bar'> hello </div>"
-      expect(subject.parser_script(string)[1]).to eq([" hello "])
+      expect(subject.html_to_array(string)[1]).to eq([" hello "])
+    end
+
+    it "parses two tags" do
+      string = "<p></p>"
+      expect(subject.html_to_array(string)).to eq([["<p>"], ["</p>"]])
+    end
+
+    it "parses 4 children" do
+      string = "<h1><p>hello</p><p>bye</p></h1>"
+      expect(subject.html_to_array(string)).to eq([["<h1>"], ["<p>"], ["hello"], ["</p>"], ["<p>"], ["bye"], ["</p>"], ["</h1>"]])
     end
 
   end
 
-  describe "#parse_tag" do 
-    it "returns a hash tag type and attributes" do 
+  describe "#parse_tag" do
+    it "returns a hash tag type and attributes" do
       tag = "<p class=\"foo bar\" id='baz' src = 'hello' >"
       expect(subject.parse_tag(tag).keys).to eq([:type, :class, :id, :src])
     end
@@ -38,27 +52,112 @@ describe DomParser do
 
   end
 
-  describe "#closing_tag?" do 
-    it "validates a normal closing tag" do
-      tag = "</a>"
-      expect(subject.closing_tag?(tag)).to be(true)
+  describe "#tag?" do
+    let(:node) { Node.new("<p>", nil, nil)}
+    it "returns true if node data is an html tag" do
+      expect(subject.tag?(node)).to be true
     end
 
-    it "returns false when passed an opening tag" do
-      tag = "<a>"
-      expect(subject.closing_tag?(tag)).to be(false)
+    it "returns false if node data is text" do
+      node = Node.new("hello", nil, nil)
+      expect(subject.tag?(node)).to be false
     end
   end
 
-  describe "#opening_tag?" do
-    it "returns true when passed an opening tag" do
-      tag = "<a>"
-      expect(subject.opening_tag?(tag)).to be(true)
+  describe "#closing?" do
+    it "returns true if passed a closing tag" do
+      expect(subject.closing?(closing)).to be true
     end
 
-    it "returns false when passed an closing tag" do
-      tag = "</a>"
-      expect(subject.opening_tag?(tag)).to be(false)
+    it "returns false if passed an opening tag" do
+      expect(subject.closing?(opening)).to be false
+    end
+
+    it "returns false if text is passed in" do
+      expect(subject.closing?(text)). to be false
+    end
+  end
+
+  describe "#text?" do
+    it "returns true if node is not a tag" do
+      expect(subject.text?(text)).to be true
+    end
+  end
+
+  describe "#create_tree" do
+    let(:one_tag) { "<p></p>"}
+    let(:two_tag) { "<p><a></a></p>"}
+    let(:one_tag_text) { "<h1>hello world</h1>"}
+    let(:two_childs) { "<h1><p></p><p></p></h1>"}
+    let(:two_childs_with_text) { "<h1><p>hello</p><p>bye</p></h1>"}
+
+
+    it "sets the root node to the first tag in the string" do
+      subject.create_tree(one_tag)
+      expect(subject.root.data).to eq("<p>")
+      expect(subject.root.children).to eq([])
+    end
+
+    it "creates a child node for the second tag in the string" do
+      subject.create_tree(two_tag)
+      expect(subject.root.children[0].data).to eq("<a>")
+      expect(subject.root.children[0].parent.data).to eq("<p>")
+    end
+
+    it "creates a child node for a text element" do
+      subject.create_tree(one_tag_text)
+      expect(subject.root.children[0].data).to eq("hello world")
+      expect(subject.root.children[0].parent.data).to eq("<h1>")
+      expect(subject.root.children.count).to eq(1)
+    end
+
+    it "creates a child node for each child element" do
+      subject.create_tree(two_childs)
+      expect(subject.root.children.count).to eq(2)
+
+    end
+
+    it "creates a child node for each child element" do
+      subject.create_tree(two_childs_with_text)
+      puts "kids are"
+      p subject.root.children
+      expect(subject.root.children.count).to eq(2)
+
+    end
+
+  end
+
+  describe "#create_node" do
+    it "receives a tag and creates a node" do
+      node = subject.create_node(["<p>"])
+      expect(node.data).to eq("<p>")
+      expect(node.parent).to eq(nil)
+      expect(node.children).to eq([])
     end
   end
 end
+
+
+# describe "#closing_tag?" do
+#   it "validates a normal closing tag" do
+#     tag = "</a>"
+#     expect(subject.closing_tag?(tag)).to be(true)
+#   end
+
+#   it "returns false when passed an opening tag" do
+#     tag = "<a>"
+#     expect(subject.closing_tag?(tag)).to be(false)
+#   end
+# end
+
+# describe "#opening_tag?" do
+#   it "returns true when passed an opening tag" do
+#     tag = "<a>"
+#     expect(subject.opening_tag?(tag)).to be(true)
+#   end
+
+#   it "returns false when passed an closing tag" do
+#     tag = "</a>"
+#     expect(subject.opening_tag?(tag)).to be(false)
+#   end
+# end
